f24303abf8092651a11bc04b5c452a9b1a9ac11c
Matias Daneri <matiasdaneri@MacBook-Pro-de-Matias.local>
2024-03-14 12:21:53 -0300

    Esta hasta el 2b

--- TP0/main.py
+++ TP0/main.py
@@ -1,10 +1,83 @@
 from src.catching import attempt_catch
 from src.pokemon import PokemonFactory, StatusEffect
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
+from src.catching import attempt_catch
+
+def ejercicio2a():
+    factory = PokemonFactory("pokemon.json")
+    pokemon_names = ["snorlax", "jolteon", "caterpie", "onix", "mewtwo"]
+    pokeball_names = ["heavyball", "ultraball", "fastball", "pokeball"]
+    status_effects = [StatusEffect.NONE, StatusEffect.POISON, StatusEffect.BURN, StatusEffect.PARALYSIS, StatusEffect.SLEEP, StatusEffect.FREEZE]
+
+    N = 100
+
+    fig, ax = plt.subplots()
+
+    bar_width = 0.2
+    index = np.arange(len(pokemon_names))
+
+    for i, pokeball_name in enumerate(pokeball_names):
+        probabilities_hp_avg = []
+        for pokemon_name in pokemon_names:
+            probabilities_hp = []
+            for status_effect in status_effects:
+                for _ in range(N):
+                    success, probability = attempt_catch(factory.create(pokemon_name, 100, status_effect, 1), pokeball_name)
+                    probabilities_hp.append(probability)
+            probabilities_hp_avg.append(np.mean(probabilities_hp))
+
+        ax.bar(index + i * bar_width, probabilities_hp_avg, bar_width, label=pokeball_name)
+
+    ax.set_xlabel('Pokemon')
+    ax.set_ylabel('Probabilidad de Captura Promedio')
+    ax.set_title('Probabilidad de Captura Promedio por Pokemon y Pokebola')
+    ax.set_xticks(index + 0.6)
+    ax.set_xticklabels(pokemon_names)
+    ax.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+    
+def ejercicio2b():
+    factory = PokemonFactory("pokemon.json")
+    pokeball_names = ["heavyball", "ultraball", "fastball", "pokeball"]
+
+    N = 100
+    hp_values = np.arange(0.0, 1.01, 0.2)  # Valores de puntos de vida (0.0 a 1.0, dividido en 0.01)
+
+    fig, ax = plt.subplots()
+    pokemon_name = "snorlax"
+
+    status_effect = StatusEffect.NONE  # Usar un solo estado de salud
+
+    bar_width = 0.2
+    index = np.arange(len(hp_values))
+
+    for i, pokeball_name in enumerate(pokeball_names):
+        probabilities_hp_avg = []
+        for hp_percentage in hp_values:
+            probabilities_hp = []
+            for _ in range(N):
+                success, probability = attempt_catch(factory.create(pokemon_name, 100, status_effect, hp_percentage), pokeball_name)
+                probabilities_hp.append(probability)
+            probabilities_hp_avg.append(np.mean(probabilities_hp))
+
+        ax.bar(index + i * bar_width, probabilities_hp_avg, bar_width, label=pokeball_name)
+
+    ax.set_xlabel('Porcentaje de Puntos de Vida')
+    ax.set_ylabel('Probabilidad de Captura Promedio')
+    ax.set_title(f'Efecto de los Puntos de Vida en la Probabilidad de Captura - {pokemon_name.capitalize()}')
+    ax.set_xticks(index + 0.3)
+    ax.set_xticklabels([f"{int(hp*100)}%" for hp in hp_values])
+    ax.legend()
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
-    factory = PokemonFactory("pokemon.json")
-    snorlax = factory.create("snorlax", 100, StatusEffect.NONE, 1)
-    print("No noise: ", attempt_catch(snorlax, "heavyball"))
-    for _ in range(10):
-        print("Noisy: ", attempt_catch(snorlax, "heavyball", 0.15))
+    ejercicio2a()
+    ejercicio2b()
--- TP0/main_mati.py
+++ TP0/main_mati.py
@@ -1,8 +1,10 @@
 import os
 import json
 from src.catching import attempt_catch
+import matplotlib.pyplot as plt
 from src.pokemon import PokemonFactory, StatusEffect
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 
 
@@ -27,12 +29,65 @@ def initialize(filePath):
 
     pokemons = [jolteon, caterpie, snorlax, onix, mewtwo]
     pokeballs = ["pokeball", "ultraball", "fastball", "heavyball"]
+    status_effects = [StatusEffect.NONE, StatusEffect.POISON, StatusEffect.BURN, StatusEffect.PARALYSIS, StatusEffect.SLEEP, StatusEffect.FREEZE]
 
-    return (pokemons, pokeballs)
+    return (pokemons, pokeballs, status_effects)
+
+def ejercicio1a():
+    pokemons, pokeballs, _ = initialize("config/ejercicio1a-config.json")
+
+    # Creamos y llenamos el DataFrame
+    df = pd.DataFrame(columns=['pokemon', 'pokeball', 'success'])
+    for pokemon in pokemons:
+        for pokeball in pokeballs:
+            for _ in range(N):
+                success, _ = attempt_catch(pokemon, pokeball)
+                new_row = {'pokemon': pokemon.name, 'pokeball': pokeball, 'success': success}
+                df.loc[len(df)] = new_row
+
+    # Calcular la media de atrapadas para cada Pokémon y pokebola.
+    media_por_pokemon_pokeball = df.groupby(['pokemon', 'pokeball'])['success'].mean().reset_index()
+    
+    # Media para cada pokemon.
+    media_por_pokemon = df.groupby(['pokemon'])['success'].mean().reset_index() 
+
+    # Crear un gráfico de barras con Plotly Express
+    fig = px.bar(media_por_pokemon_pokeball, x='pokemon', y='success', color='pokeball', barmode='group', title='Promedio de Atrapadas por Pokémon y Tipo de Pokeball')
+    fig.add_trace(go.Scatter(x=media_por_pokemon['pokemon'], y=media_por_pokemon['success']))
+
+    # Mostrar el gráfico
+    fig.show()
 
 
+def ejercicio1a_corregido():
+    pokemons, pokeballs, _ = initialize("config/ejercicio1a-config.json")
+
+    # Creamos y llenamos el DataFrame
+    df = pd.DataFrame(columns=['pokemon', 'pokeball', 'success'])
+    for pokemon in pokemons:
+        for pokeball in pokeballs:
+            for _ in range(N):
+                success, _ = attempt_catch(pokemon, pokeball)
+                new_row = {'pokemon': pokemon.name, 'pokeball': pokeball, 'success': success}
+                df.loc[len(df)] = new_row
+
+    # Calcular la media de atrapadas para cada Pokémon y pokebola.
+    media_por_pokeball_pokemon = df.groupby(['pokeball', 'pokemon'])['success'].mean().reset_index()
+    
+    # Media para cada pokemon.
+    media_por_pokeball = df.groupby(['pokeball'])['success'].mean().reset_index() 
+
+    # Crear un gráfico de barras con Plotly Express
+    fig = px.bar(media_por_pokeball_pokemon, x='pokeball', y='success', color='pokemon', barmode='group', title='Promedio de Atrapadas por Pokémon y Tipo de Pokeball')
+    fig.add_trace(go.Scatter(x=media_por_pokeball['pokeball'], y=media_por_pokeball['success']))
+
+    # Mostrar el gráfico
+    fig.show()
+
+
+    
 def ejercicio1b():
-    pokemons, pokeballs = initialize("config/ejercicio1b-config.json")
+    pokemons, pokeballs, _ = initialize("config/ejercicio1b-config.json")
 
     # Creamos y llenamos el DataFrame
     df = pd.DataFrame(columns=['pokemon', 'pokeball', 'success'])
@@ -74,27 +129,84 @@ def ejercicio1b():
     fig.show()
 
 
-def ejercicio1a():
-    pokemons, pokeballs = initialize("config/ejercicio1a-config.json")
+def ejercicio2a():
+    pokemons, pokeballs, status_effects = initialize("config/ejercicio2a-config.json")
+    factory = PokemonFactory("pokemon.json")
 
-    # Creamos y llenamos el DataFrame
-    df = pd.DataFrame(columns=['pokemon', 'pokeball', 'success'])
+    df = pd.DataFrame(columns=['pokemon', 'pokeball', 'status', 'success'])
     for pokemon in pokemons:
         for pokeball in pokeballs:
-            for _ in range(N):
-                success, _ = attempt_catch(pokemon, pokeball)
-                new_row = {'pokemon': pokemon.name, 'pokeball': pokeball, 'success': success}
-                df.loc[len(df)] = new_row
+            for current_status in status_effects:
+                pokemon = factory.create(pokemon.name, pokemon.level, current_status, 1)
+                for _ in range(N):
+                    success, _ = attempt_catch(pokemon, pokeball)
+                    new_row = {'pokemon': pokemon.name, 'pokeball': pokeball, 'status': current_status.name, 'success': success}
+                    df.loc[len(df)] = new_row
 
-    # Calcular la media de atrapadas para cada Pokémon
-    media_por_pokemon_pokeball = df.groupby(['pokemon', 'pokeball'])['success'].mean().reset_index()
+#    df = df.groupby(['pokemon', 'status'])['success'].sum()
+
+    # Agrupar los datos por Pokémon y estado, y calcular la tasa de éxito promedio
+    grouped_df = df.groupby(['pokemon', 'status'])['success'].mean().reset_index()
     
-    # Crear un gráfico de barras con Plotly Express
-    fig = px.bar(media_por_pokemon_pokeball, x='pokemon', y='success', color='pokeball', barmode='group', title='Promedio de Atrapadas por Pokémon y Tipo de Pokeball')
-    
+
+    # Crear el gráfico de barras agrupadas
+    fig = px.bar(grouped_df, x='status', y='success', color='pokemon',
+                title='Efectividad de Captura por Pokémon y Estado',
+                barmode='group',
+                labels={'success': 'Media de Atrapadas', 'status': 'Estado'})
+
+    # Agregar trazos de líneas para cada Pokémon
+    for pokemon in grouped_df['pokemon'].unique():
+        pokemon_data = grouped_df[grouped_df['pokemon'] == pokemon]
+        fig.add_trace(go.Scatter(x=pokemon_data['status'], y=pokemon_data['success'], mode='lines', name=pokemon))
+
     # Mostrar el gráfico
     fig.show()
+
+
+def ejercicio2b():
+    factory = PokemonFactory("pokemon.json")
+    pokeballs = ["pokeball", "ultraball", "fastball", "heavyball"]
+
+    with open("config/ejercicio2b-config.json", "r") as file:
+        config = json.load(file)
     
+    df_list = []
+    hp_steps = [x * 0.1 for x in range(0, 11)]
+    for current in config:
+        df = pd.DataFrame(columns=['pokemon', 'pokeball', 'hp', 'success'])
+        for hp in hp_steps:
+            pokemon = factory.create(current['pokemon'], current['level'], StatusEffect[current["status"]], hp)
+            
+            for _ in range(N):
+                for pokeball in pokeballs:
+                    success, _ = attempt_catch(pokemon, pokeball)
+                    new_row = {'pokemon': pokemon.name, 'pokeball': pokeball, 'hp': hp, 'success': success}
+                    df.loc[len(df)] = new_row
+            
+        # Para un mismo pokemon.
+        media_base_por_hp = df[df.pokeball == 'pokeball']['success'].mean()
+
+        # Eficiencia como promedio de la pokebola básica
+        media_por_hp = df.groupby('hp')['success'].mean() / media_base_por_hp
+
+        # Agregar los datos al DataFrame de la lista
+        df['efficiency'] = df['hp'].map(media_por_hp)
+        df_list.append(df)
+
+    # Concatenar todos los DataFrames en uno solo
+    final_df = pd.concat(df_list)
+
+    # Graficar con Plotly Express
+    fig = px.line(final_df, x='hp', y='efficiency', color='pokemon', line_group='pokemon', labels={'efficiency': 'Eficiencia', 'hp': 'HP', 'pokemon': 'Pokemon'}, title='Eficiencia de captura por HP')
+    fig.show()
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    ejercicio1b()
+    ejercicio2b()
