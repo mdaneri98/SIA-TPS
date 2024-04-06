e4d6b582502182685c1080a2751231fd1fce61db
dlimachi <dlimachi@itba.edu.ar>
2024-04-06 14:08:34 -0300

    Added end conditions

---
+++ TP2/end_condition.py
@@ -0,0 +1,19 @@
+
+def avg_performance(population):
+    return sum([character.get_performance() for character in population]) / len(population)
+
+def check_content(population, previous_population, delta):
+    pop_avg_performance = avg_performance(population)
+    previous_pop_avg_performance = avg_performance(previous_population)
+
+    return abs(pop_avg_performance - previous_pop_avg_performance) < delta
+
+def check_optimal_fitness(population, optimal_fitness, optimal_fitness_error):
+    best_character = max(population, key=lambda character: character.get_performance())
+    best_character_performance =  best_character.get_performance()
+    return (optimal_fitness - optimal_fitness_error) <= best_character_performance <= (optimal_fitness + optimal_fitness_error)
+
+def check_max_generation(generation, max_generations): 
+    if (generation >= max_generations):
+        return True
+ 
