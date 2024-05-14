0e604e435869f42ff93a2d7bbc715ea549d92338
josephrqt <josephrouquette@gmail.com>
2024-05-13 21:41:33 -0300

    delele mainC

--- TP3/ejercicio3/mainB.py
+++ TP3/ejercicio3/mainB.py
@@ -1,19 +1,17 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+from neural_network import NeuralNetwork
 from Activation import Sigmoid
 from Optimazer import *
-from ejercicio3.neural_network import NeuralNetwork
-
 
 def split_data(data, labels, train_ratio):
     indices = np.arange(len(data))
-    np.random.seed(42)
+    np.random.seed(43)
     np.random.shuffle(indices)
     train_size = int(len(data) * train_ratio)
     train_indices = indices[:train_size]
     test_indices = indices[train_size:]
     return np.array(data)[train_indices], np.array(data)[test_indices], np.array(labels)[train_indices], np.array(labels)[test_indices]
+
 
 def read_data(archivo):
     with open(archivo) as file:
@@ -26,75 +24,133 @@ def read_data(archivo):
     return matrices
 
 
-def accuracy_for_model(model, x_test, y_test, eps):
-    predictions = model.predict(x_test)
-    predicted_digits = [np.round(np.argmax(output)) for output in predictions]
-    total_rights = np.sum([np.abs(pred - real) < eps for pred, real in zip(predicted_digits, y_test)])
-    return total_rights / len(x_test)
+archive = "TP3-ej3-digitos.txt"
 
-def accuracy_for_with_std(model_params, data, labels, training_percentage, num_trials=10):
-    accuracies = []
-    X_train, X_test, y_train, y_test = split_data(data, labels, training_percentage)
-    for _ in range(num_trials):
-        dim, hidden_size, output_size, learning_rate, epochs, eps, optimizer = model_params
-        model = NeuralNetwork([dim, hidden_size, output_size], Sigmoid(), optimizer, verbose=False)
-        model.train(X_train, y_train, epochs)
-        acc = accuracy_for_model(model, data, labels, eps)
-        accuracies.append(acc)
-    mean_accuracy = np.mean(accuracies)
-    std_dev = np.std(accuracies)
-    return mean_accuracy, std_dev
-
-# Datos y configuración
-matrices = [np.array(matrix).flatten() for matrix in read_data("TP3-ej3-digitos.txt")]
+matrices = [np.array(matrix).flatten() for matrix in read_data(archive)]
 expected_output = np.array([[1], [0], [1], [0], [1], [0], [1], [0], [1], [0]])
+#output is 1 if it s a mutiply of 2 and -1 whereas ; the values are in [-1;1]
 
-dim = len(matrices[0])
-hidden_size = 40
+input_size = len(matrices[0])
+hidden_size = 15
 output_size = 1
-learning_rate = 0.01
-eps = 0.1
-epochs = 1000
-training_percentages = [0.2, 0.4, 0.6, 0.8]
+epochs = 5000
 
-# Optimizador ADAM
-optimizer = GradientDescentOptimizer(learning_rate=0.01)
-accuracies_with_std = []
-for pct in training_percentages:
-    model_params = (dim, hidden_size, output_size, learning_rate, epochs, eps, optimizer)
-    accuracy = accuracy_for_with_std(model_params, matrices, expected_output, pct)
-    accuracies_with_std.append(accuracy)
+def errors_training_learning_rate(opti):
+    
+    training_percentage = 1.0
+    run_number = 5
+    learning_rates = [0.1, 0.01, 0.001]
+    plt.figure()  # Créer une nouvelle figure pour le graphique global
+    for learning_rate in learning_rates:
+        all_errors = []
+        for run in range(1, run_number + 1):
+            X_train, X_test, y_train, y_test = split_data(matrices, expected_output, training_percentage)
+            if (opti == "Adam"):
+                optimizer = AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=0.01)
+            else : 
+                optimizer = GradientDescentOptimizer(learning_rate)
+            model = NeuralNetwork([input_size, hidden_size, output_size], Sigmoid(), optimizer, verbose=False)
+            errors = model.train(X_train, y_train, epochs)
+            all_errors.append(errors)
+        mean_errors = np.mean(all_errors, axis=0)
+        plt.plot(np.arange(1, len(mean_errors) + 1), mean_errors, label='Learning Rate: {}'.format(learning_rate))
+
+    plt.yscale('log')
+    plt.xlabel('Iterations')
+    plt.ylabel('Errors')
+    plt.title('Evolution of Errors medium during depending several learning rate on 5 runs')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def errors_test_training_rate(opti,archi):
+    learning_rate = 0.1
+    run_number = 5
+    training_percentages = [0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80]
+
+    mean_errors = []  # Pour stocker les erreurs moyennes pour chaque pourcentage d'entraînement
+
+    for training_percentage in training_percentages:
+        all_errors = []
+        for run in range(1, run_number + 1):
+            X_train, X_test, y_train, y_test = split_data(matrices, expected_output, training_percentage)
+            if (opti == "Adam"):
+                optimizer = AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=0.01)
+            else : 
+                optimizer = GradientDescentOptimizer(learning_rate)
+            model = NeuralNetwork(archi,  Sigmoid(),optimizer, verbose=False)
+            _ = model.train(X_train, y_train, epochs)
+            result = model.predict(X_test)
+            errors = [(result[i] - y_test[i])**2 for i in range(len(result))]
+            error = np.mean(errors)  # Calcul de l'erreur moyenne
+            all_errors.append(error/((1-training_percentage)*10))
+        mean_error = np.mean(all_errors)  # Calcul de l'erreur moyenne sur les 5 exécutions
+        mean_errors.append(mean_error)
+
+    # Tracer les barres
+    plt.bar(np.arange(len(training_percentages)), mean_errors)
+    plt.xlabel('Porcentaje de entrenamiento')
+    plt.ylabel('Error medio')
+    'Error medio '
+    plt.title('Error medio según porcentaje de entrenamiento (Learning Rate: {})'.format(learning_rate))
+    plt.xticks(np.arange(len(training_percentages)), [str(int(p * 100)) + '%' for p in training_percentages])
+    plt.grid(True)
+    plt.show()
 
 
-means = [acc[0] for acc in accuracies_with_std]
-std_devs = [acc[1] for acc in accuracies_with_std]
+def errors_training_optimizer(training_ratio,archi):
+    learning_rate = 0.1
+    run_number = 5
+    training_percentage = training_ratio
 
-# Crear gráfico de barras con barras de error
-plt.bar(training_percentages, means, yerr=std_devs, capsize=5, width=0.1, align='center')
-plt.xlabel("Porcentaje de Entrenamiento con optimizador ADAM.")
-plt.ylabel("Accuracy")
-plt.title("Accuracy del training vs Porcentaje de Entrenamiento")
-plt.grid(axis='y')
-plt.show()
+    plt.figure()  # Créer une nouvelle figure pour le graphique global
+    
+    all_errors = []
+    for run in range(1, run_number + 1):
 
-
-
-# Optimizador Gradiente descendente
-optimizer = GradientDescentOptimizer(learning_rate=0.01)
-accuracies_with_std = []
-for pct in training_percentages:
-    model_params = (dim, hidden_size, output_size, learning_rate, epochs, eps, optimizer)
-    accuracy = accuracy_for_with_std(model_params, matrices, expected_output, pct)
-    accuracies_with_std.append(accuracy)
+        X_train, X_test, y_train, y_test = split_data(matrices, expected_output, training_percentage)
+        optimizer = AdamOptimizer(learning_rate, beta1=0.9, beta2=0.999, epsilon=0.01)
+        model = NeuralNetwork(archi, Sigmoid(),optimizer, verbose=False)
+        errors = model.train(X_train, y_train, epochs)
+        all_errors.append(errors)
+    mean_errors = np.mean(all_errors, axis=0)
+    plt.plot(np.arange(1, len(mean_errors) + 1), mean_errors, label='Adam')
 
 
-means = [acc[0] for acc in accuracies_with_std]
-std_devs = [acc[1] for acc in accuracies_with_std]
+    all_errors = []
+    for run in range(1, run_number + 1):
 
-# Crear gráfico de barras con barras de error
-plt.bar(training_percentages, means, yerr=std_devs, capsize=5, width=0.1, align='center')
-plt.xlabel("Porcentaje de Entrenamiento con optimizador Gradiente descendente.")
-plt.ylabel("Accuracy")
-plt.title("Accuracy del training vs Porcentaje de Entrenamiento")
-plt.grid(axis='y')
-plt.show()
+        X_train, X_test, y_train, y_test = split_data(matrices, expected_output, training_percentage)
+        optimizer = GradientDescentOptimizer(learning_rate)
+        model = NeuralNetwork(archi, Sigmoid(),optimizer, verbose=False)
+        errors = model.train(X_train, y_train, epochs)
+        all_errors.append(errors)
+    mean_errors = np.mean(all_errors, axis=0)
+    plt.plot(np.arange(1, len(mean_errors) + 1), mean_errors, label='GradientDescent')
+
+    plt.yscale('log')
+    plt.xlabel('Época')
+    plt.ylabel('Errors')
+    plt.title('Evolución del error medio con {} del data set para entrenar'.format(training_percentage))
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+
+
+
+errors_training_learning_rate("Gradient")
+errors_training_learning_rate("Adam")
+ 
+errors_training_optimizer(0.20,[input_size, hidden_size, output_size])
+errors_training_optimizer(0.20,[input_size, hidden_size,15, output_size])
+errors_training_optimizer(0.50,[input_size, hidden_size, output_size])
+errors_training_optimizer(0.50,[input_size, hidden_size,15, output_size])
+errors_training_optimizer(0.80,[input_size, hidden_size, output_size])
+errors_training_optimizer(0.80,[input_size, hidden_size,15, output_size])
+
+errors_test_training_rate("Gradient",[input_size, hidden_size, output_size])
+errors_test_training_rate("Gradient",[input_size, hidden_size,15, output_size])
+errors_test_training_rate("Adam",[input_size, hidden_size, output_size])
+errors_test_training_rate("Adam",[input_size, hidden_size,15, output_size])
