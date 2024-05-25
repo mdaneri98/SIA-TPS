d3d538bdc6aa87237e98513a0fb1b98d4db4cb6f
dlimachi <dlimachi@itba.edu.ar>
2024-05-25 01:00:32 -0300

    cambio de graficos

--- TP4/Kohonen/main.py
+++ TP4/Kohonen/main.py
@@ -6,7 +6,6 @@ from KohonenNetwork import *
 import seaborn as sns
 
 
-
 def get_data():
     data = pd.read_csv('europe.csv')
     countries = data["Country"].tolist()
@@ -36,34 +35,26 @@ def plot_heatmap(network, input_data, countries):
         country_count[countries[i]] += 1
 
     fig, ax = plt.subplots()
-    cax = ax.matshow(activation_matrix, cmap='viridis')
+    cax = ax.matshow(activation_matrix, cmap='viridis', origin='lower', extent=[0, k, 0, k])
     ax.set_title('Heatmap of Countries')
-    ax.set_xticks(np.arange(k + 1))
-    ax.set_yticks(np.arange(k + 1))
-    ax.set_xticklabels(list(range(k + 1)))
-    ax.set_yticklabels(list(range(k + 1)))
+    ax.set_xticks(np.arange(k))
+    ax.set_yticks(np.arange(k))
+    ax.set_xticklabels(np.arange(1, k + 1))  # Ajusta las etiquetas del eje x
+    ax.set_yticklabels(np.arange(1, k + 1))  # Ajusta las etiquetas del eje y
 
     for i in range(k):
         for j in range(k):
-            country_name = ''
-            for country, position in zip(countries, input_data):
-                bmu_position, similarity = network.predict(position)
-                max_index = similarity.argmax()
-                x = max_index // network.k  # División entera para obtener la fila
-                y = max_index % network.k
-                if x == i and y == j:
-                    country_name += f'{country}\n'
-            ax.text(j, i, activation_matrix[i, j], ha='center', va='center', color='black')
+            text = activation_matrix[i, j]
+            ax.text(j + 0.5, i + 0.5, text, ha='center', va='center', color='black')
 
     cbar = fig.colorbar(cax)
-    cbar.set_label('Frequency')
 
     plt.show()
 
 
 def plot_average_neighbor_distances(ud_matrix, k):
     cmap = plt.get_cmap('gray')
-    plt.imshow(ud_matrix, cmap=cmap)
+    plt.imshow(ud_matrix, cmap=cmap, extent=[0, k, 0, k], origin='lower')
 
     min_val = min(min(row) for row in ud_matrix)
     max_val = max(max(row) for row in ud_matrix)
@@ -72,28 +63,10 @@ def plot_average_neighbor_distances(ud_matrix, k):
     for i in range(k):
         for j in range(k):
             color = 'w' if ud_matrix[i][j] < color_thr else 'k'
-            plt.text(j, i, str(f"{ud_matrix[i][j]:.2f}"), ha='center', va='center', color=color)
+            plt.text(j + 0.5, i + 0.5, "", ha='center', va='center', color=color)
 
     cbar = plt.colorbar()
-    cbar.set_label('Valor')
 
-    plt.show()
-
-
-
-def plot_neighbor_distances(network, radius):
-    all_distances = []
-
-    for i in range(network.k):
-        for j in range(network.k):
-            position = (i, j)
-            neighbors = network.get_neighbors(position, radius)
-            all_distances.extend(dist for _, dist in neighbors)
-
-    plt.hist(all_distances, bins=20)
-    plt.xlabel('Distance')
-    plt.ylabel('Frequency')
-    plt.title('Distribution of Distances to Neighboring Neurons')
     plt.show()
 
 
@@ -113,53 +86,45 @@ def plot_confusion_matrix(association_count):
     plt.title("Matriz de Confusión")
     plt.show()
 
-# Función para entrenar la red de Kohonen y analizar la asociación para una variable específica
+
 def analyze_variable(network, input_data, variable_index, variables):
-    network.train(num_epochs=100)
+    k = network.k
     association_count = analyze_association(network, input_data[:, [variable_index]])
 
     plt.figure(figsize=(8, 6))
-    for i in range(association_count.shape[0]):
-        for j in range(association_count.shape[1]):
-            plt.text(j, i, f"{association_count[i, j]:.2f}", ha='center', va='center', color='black')
-    plt.imshow(association_count, cmap='Blues')
+    plt.imshow(association_count, cmap='Blues', origin='lower', extent=[0, k, 0, k], vmin=association_count.min())
     plt.colorbar()
-    plt.title(f'Asociación de elementos a cada neurona para la variable "{variables[variable_index]}"')
-    plt.xlabel("Columna de Neurona")
-    plt.ylabel("Fila de Neurona")
+    plt.title(f'Heatmap for "{variables[variable_index]}"')
+
     plt.show()
 
 
 def main():
     countries, labels, data = get_data()
-
-    standarized_data = standarize_data(data)
-
-    k = 8
-    learning_rate = 0.01
-    initial_radius = 3
-    max_epochs = 1000
-
-    network = KohonenNetwork(standarized_data, len(standarized_data[0]), k, learning_rate, initial_radius)
-
-    network.train(max_epochs)
-
-    plot_heatmap(network, standarized_data, countries)
-
-    plot_neighbor_distances(network, initial_radius)
-
-    plot_average_neighbor_distances(network.calculate_unified_distances(initial_radius), k)
-
-    association_count = analyze_association(network, standarized_data)
-    print("Cantidad de elementos asociados a cada neurona:")
-    print(association_count)
-    plot_confusion_matrix(association_count)
-
+    standard_data = standarize_data(data)
     variables = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment']
 
-    # Graficar los resultados para cada variable
+    k = 4
+    learning_rate = 0.01
+    initial_radius = 2
+    max_epochs = 1000
+
+    # Network 4x4
+    network = KohonenNetwork(standard_data, len(standard_data[0]), k, learning_rate, initial_radius)
+    network.train(max_epochs)
+
+    plot_heatmap(network, standard_data, countries)
+    plot_average_neighbor_distances(network.calculate_unified_distances(initial_radius), k)
     for i, variable in enumerate(variables):
-        analyze_variable(network, standarized_data, i, variables)
+        analyze_variable(network, standard_data, i, variables)
+
+    # Network 5x5
+    k = 5
+    network2 = KohonenNetwork(standard_data, len(standard_data[0]), k, learning_rate, initial_radius)
+    network2.train(max_epochs)
+    plot_heatmap(network2, standard_data, countries)
+    plot_average_neighbor_distances(network2.calculate_unified_distances(initial_radius), k)
+
 
 if __name__ == "__main__":
     main()
